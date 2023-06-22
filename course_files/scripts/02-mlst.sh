1f6d138c8772c22d31a505c258b0f39c7740f168
bsalehe <bajuna.salehe@gmail.com>
2023-06-22 11:56:03 +0100

    Update sequence typing script

--- course_files/scripts/02-mlst.sh
+++ course_files/scripts/02-mlst.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-mkdir results/mlst
+#mkdir results/mlst
 
-for file in results/wf-bacterial-genomes/new/barcode*.medaka.fasta
-do
-    mlst $file > results/mlst/$(basename $file .medaka.fasta).tsv
-done
+#for file in results/wf-bacterial-genomes/new/barcode*.medaka.fasta
+#do
+#    mlst $file > results/mlst/$(basename $file .medaka.fasta).tsv
+#done
+
+#cd results/mlst/
+#cat * > results/mlst/cholera_beirut_mlst.tsv
+
+mlst --scheme vcholerae results/wf-bacterial-genomes/new/*.fasta > results/mlst/cholera_beirut_mlst.tsv
