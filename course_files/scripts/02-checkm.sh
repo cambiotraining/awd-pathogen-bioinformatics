4743afcda9f13127acfb37318308f6c329103933
bsalehe <bajuna.salehe@gmail.com>
2023-06-23 15:17:33 +0100

    Modified script

--- course_files/scripts/02-checkm.sh
+++ course_files/scripts/02-checkm.sh
@@ -8,6 +8,8 @@
 # tar xfz checkm_data_2015_01_16.tar.gz
 # checkm data setRoot $(pwd)
 #
-# mkdir -p results/checkm
+mkdir -p results/checkm
+
+# Use -x to specify the format of input fasta files. By default it looks for .fna formats in the directory
 
 checkm lineage_wf results/wf-bacterial-genomes/new/ results/checkm --tab_table -t 8 -x fasta --reduced_tree > results/checkm/sudan_samples_completeness.tsv
