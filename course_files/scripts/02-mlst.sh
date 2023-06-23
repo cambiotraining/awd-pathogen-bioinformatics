0a94cce4bd5f185970140d413211d9dca3ad14ad
bsalehe <bajuna.salehe@gmail.com>
2023-06-23 15:18:04 +0100

    Modified script

--- course_files/scripts/02-mlst.sh
+++ course_files/scripts/02-mlst.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#mkdir results/mlst
-
-#for file in results/wf-bacterial-genomes/new/barcode*.medaka.fasta
-#do
-#    mlst $file > results/mlst/$(basename $file .medaka.fasta).tsv
-#done
-
-#cd results/mlst/
-#cat * > results/mlst/cholera_beirut_mlst.tsv
+mkdir results/mlst
 
 mlst --scheme vcholerae results/wf-bacterial-genomes/new/*.fasta > results/mlst/cholera_beirut_mlst.tsv
