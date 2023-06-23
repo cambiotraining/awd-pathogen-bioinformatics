ed504b0bd2097a258bf574c3ab4c5c7c5a130378
bsalehe <bajuna.salehe@gmail.com>
2023-06-23 14:58:20 +0100

    Added checkm script

---
+++ course_files/scripts/02-checkm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setting up databases for checkM
+#
+# mkdir -p data/checkM_databases
+# cd data/checkM_databases
+# wget https://data.ace.uq.edu.au/public/CheckM_databases/checkm_data_2015_01_16.tar.gz
+# tar xfz checkm_data_2015_01_16.tar.gz
+# checkm data setRoot $(pwd)
+#
+# mkdir -p results/checkm
+
+checkm lineage_wf results/wf-bacterial-genomes/new/ results/checkm --tab_table -t 8 -x fasta --reduced_tree > results/checkm/sudan_samples_completeness.tsv
