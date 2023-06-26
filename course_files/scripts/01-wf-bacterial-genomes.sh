9af9ea9bf9fda0eee439e3c2b5c77587692fecc7
bsalehe <bajuna.salehe@gmail.com>
2023-06-26 14:54:58 +0100

    Updated assembly script

--- course_files/scripts/01-wf-bacterial-genomes.sh
+++ course_files/scripts/01-wf-bacterial-genomes.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+#mkdir -p results/wf-bacterial-genomes
 
-nextflow run epi2me-labs/wf-bacterial-genomes -resume \
+nextflow run epi2me-labs/wf-bacterial-genomes -resume -profile singularity \
 --sample_sheet sample_sheet.csv \
 --fastq data/fastq_pass/clean \
---reference 'reference_genomes/vibriocholerae/GCA_017948285.1_ASM1794828v1_genomic.fna' \
+--reference 'reference_genomes/vibriocholerae/GCF_017948285.1_ASM1794828v1_genomic.fna' \
 --threads 4 \
---out_dir 'results/wf-bacterial-genomes/new' \
+--out_dir 'results/wf-bacterial-genomes' \
 --reference_based_assembly True \
 --isolates True \
 --species 'other' 
 
 
-mv output results/wf-bacterial-genomes/new/
+mv output results/wf-bacterial-genomes/
