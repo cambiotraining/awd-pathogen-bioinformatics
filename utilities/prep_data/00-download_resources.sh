bfb776afc4144b7fbdd774061107d83e99d34ed4
Hugo Tavares <hm533@cam.ac.uk>
2023-08-02 16:40:02 +0100

    add gff and convert to prokka

--- utilities/prep_data/00-download_resources.sh
+++ utilities/prep_data/00-download_resources.sh
@@ -26,14 +26,41 @@ datasets download genome taxon 666 \
   --annotated \
   --assembly-level complete \
   --assembly-source 'RefSeq' \
-  --include genome \
+  --include genome,gff3 \
   --exclude-atypical \
   --released-after 01/01/2019
 
-# unzip and tidy
+# unzip
 unzip ncbi_dataset.zip
+
+# convert files to panaroo
+conda activate awd
+wget https://raw.githubusercontent.com/gtonkinhill/panaroo/master/scripts/convert_refseq_to_prokka_gff.py
+
+for genome in ncbi_dataset/data/*/*_genomic.fna
+do
+  fname=$(basename $genome .fna)
+  dname=$(dirname $genome)
+  
+  # convert GFF
+  python3 convert_refseq_to_prokka_gff.py \
+    -g ${dname}/genomic.gff \
+    -f $genome \
+    -o resources/vibrio_genomes/${fname}.gff
+    
+  echo "Finished $fname"
+done
+
+# move FASTAs
 mv ncbi_dataset/data/*/*.fna resources/vibrio_genomes/
-rm -r ncbi_dataset ncbi_dataset.zip README.md
+
+# remove unnecessary files
+rm -r ncbi_dataset ncbi_dataset.zip README.md convert_refseq_to_prokka_gff.py
+
+# move genome to be used as reference 
+# sequenced by Sanger from 2019 wave 3 O1 El Tor
+mkdir resources/reference_genome/
+cp resources/vibrio_genomes/GCF_937000105.1_CNRVC190243_genomic.fna resources/reference_genome
 
 
 ### checkM database ###
